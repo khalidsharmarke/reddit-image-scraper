ee7c0fc563bcd0e4ab2368b0eb671ff137d16e83
khalidsharmarke <ksharmarke972@gmail.com>
2020-07-17 12:39:02 -0400

    set max to 100

--- sanitize-validate.js
+++ sanitize-validate.js
@@ -10,10 +10,10 @@ function validateRequest(req, res, next) {
     } else if (isNaN(req.body.num_of_images)) {
         res.status(400).send('please request a number of images as an integer')
         return
-    }else if (req.body.num_of_images < 1){
+    } else if (req.body.num_of_images < 1) {
         res.status(400).send('please request a number of images larger than 1')
         return
-    }else if (req.body.num_of_images % 1 != 1){
+    } else if (req.body.num_of_images % 1 != 1) {
         res.status(400).send('please request a number of images as an integer')
         return
     }
@@ -26,7 +26,7 @@ function validateRequest(req, res, next) {
         res.status(400).send('please omit any queries from url')
         return
     } else if (request.hash) {
-        res.status(400).send('please omit any hashes from url') 
+        res.status(400).send('please omit any hashes from url')
         return
     } else if (request.search) {
         res.status(400).send('please omit any searches from url')
@@ -40,13 +40,14 @@ function validateRequest(req, res, next) {
     } else if (!request.path.includes('/r/')) {
         res.status(400).send('please request a specific subreddit')
         return
-    } else if(request.path == '/r/'){
+    } else if (request.path == '/r/') {
         res.status(400).send('please request a specific subreddit')
         return
-    }else {
-        req.locals = { ...request, 
-            num_of_images: req.body.num_of_images, 
-            mobile: req.query.mobile }
+    } else {
+        req.locals = { ...request,
+            num_of_images: req.body.num_of_images,
+            mobile: req.query.mobile
+        }
         next()
     }
 }
@@ -60,13 +61,13 @@ function sanitizeRequest(req, res, next) {
         path = path.slice(0, path.length - 1)
     }
 
-    if (num_of_images > 50) {
-        num_of_images = 50
+    if (num_of_images > 100) {
+        num_of_images = 100
     }
 
-    if (mobile == 'true'){
+    if (mobile == 'true') {
         mobile = true
-    }else {
+    } else {
         mobile = false
     }
 
